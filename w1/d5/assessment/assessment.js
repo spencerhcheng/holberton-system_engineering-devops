7e6e7f7c631575653aac3ae18e4973581ba716d4
ailadson <aladsonjr@gmail.com>
2016-01-24 23:13:42 -0800

    day 4 and some of the assessment

---
+++ w1/d5/assessment/assessment.js
@@ -0,0 +1,28 @@
+function printRange(start, end){
+  // your code here...
+}
+
+function unique(array){
+  // your code here...
+}
+
+function elementCount(array){
+  // your code here...
+}
+
+function reverseSentence(sentence){
+  // your code here...
+}
+
+function fizzBuzz(max){
+  // your code here...
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = {
+  printRange : printRange,
+  unique : unique,
+  elementCount : elementCount,
+  reverseSentence : reverseSentence,
+  fizzBuzz : fizzBuzz
+};
