1ed4d3c2ca32963ebe0439d40b480e7e8dcae4d3
Anthony Ira Ladson <aladsonjr@gmail.com>
2016-02-03 17:05:12 -0800

    day 3 ready

--- solutions/w1/d2/solution.js
+++ solutions/w1/d2/solution.js
@@ -51,13 +51,11 @@ function isPrime(number){
 }
 
 function isPrimeOptimized(number){
-  var prime = true;
-
   for(var i = 2; i < number; i += 1) {
     if(number % i === 0) {
       return false;
     }
   }
 
-  return prime;
+  return true;
 }
