b59046ee86e5c1c8cb9860ff6aa296841dc18fe0
Anthony Ira Ladson <aladsonjr@gmail.com>
2016-02-11 19:36:45 -0800

    w2d4 solution

---
+++ solutions/w2/d4/solution.js
@@ -0,0 +1,62 @@
+function factorial(n){
+  var product = 1;
+
+  for (var i = 1; i <= n; i++) {
+    product *= i;
+  }
+
+  return product;
+}
+
+function longWordCount(sentence){
+  var count = 0;
+  sentence = sentence.split(" ");
+
+  for (var i = 0; i < sentence.length; i++) {
+    var word = sentence[i];
+    if(word.length > 7){
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+
+function lcm(num1, num2){
+  var i = num1;
+
+  while(true){
+    if(i % num2 === 0 ){
+      return i;
+    }
+    i += num1;
+  }
+}
+
+//----------------------------------------------------------------------------//
+
+function hipsterfyWord(word){
+  var vowels = ["a","e","i","o","u"];
+
+  for(var i = word.length - 1; i >= 0; i -= 1){
+    if(vowels.indexOf(word[i]) !== -1){
+      var hipsterWord = word.slice(0,i) + word.slice(i+1);
+      return hipsterWord;
+    }
+  }
+
+  return word;
+}
+
+function hipsterfy(sentence){
+  var hipsterified = [];
+  sentence = sentence.split(" ");
+
+  for (var i = 0; i < sentence.length; i += 1) {
+    var word = sentence[i];
+    hipsterified.push(hipsterfyWord(word));
+  }
+
+  return hipsterified.join(" ");
+}
