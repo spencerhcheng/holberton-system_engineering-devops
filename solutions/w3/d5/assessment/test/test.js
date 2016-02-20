0eb8c1a4c472955d5635387425d070e90798617b
Anthony Ira Ladson <aladsonjr@gmail.com>
2016-02-20 13:50:22 -0800

    add solutions to w3 assessment

---
+++ solutions/w3/d5/assessment/test/test.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+
+var laligatSequence = require('../assessment.js').laligatSequence,
+    inPigLatin = require('../assessment.js').inPigLatin;
+
+describe('#inPigLatin()', function () {
+  it('should translate a sentence into pig latin.', function () {
+    var result = inPigLatin("Shmanthony is the best teacher");
+    var expected = "Anthonyshmay isyay ethay estbay eachertay";
+    assert.equal(result, expected);
+
+    result = inPigLatin("let us Dance");
+    expected = "etlay usyay Anceday";
+    assert.equal(result, expected);
+
+    result = inPigLatin("this is the time of my life");
+    expected = "isthay isyay ethay imetay ofyay myay ifelay";
+    assert.equal(result, expected);
+  });
+});
+
+describe('#laligatSequence()', function () {
+  it('should return the laligat sequence starting with base and of length n.', function () {
+    var result = laligatSequence(10, 4);
+    var expected = [ 10, 17, 58, 381 ];
+    assert.deepEqual(result, expected);
+
+    result = laligatSequence(5, 2);
+    expected = [ 5, 10 ];
+    assert.deepEqual(result, expected);
+
+    result = laligatSequence(5, 0);
+    expected = [];
+    assert.deepEqual(result, expected);
+  });
+});
