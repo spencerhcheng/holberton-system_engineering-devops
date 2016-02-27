38c8795804316b1230ca2cf073d4c4c384c645dd
Winnie Ngo <ngowinn@gmail.com>
2016-02-26 17:22:25 -0800

    edit assessment

--- w4/d5/assessment/test/test.js
+++ w4/d5/assessment/test/test.js
@@ -4,14 +4,6 @@ var divisibleBy7Sort = require('../assessment.js').divisibleBy7Sort,
     inPigLatin = require('../assessment.js').inPigLatin,
     longestLetterStreak = require('../assessment.js').longestLetterStreak;
 
-describe('#longestLetterStreak()', function () {
-  it('should return the longest consecutive searchLetters found in the str.', function () {
-    assert.equal(longestLetterStreak("ACCA", ["C"]), 2);
-    assert.equal(longestLetterStreak("YACCADCA", ["C", "A"]), 4);
-    assert.equal(longestLetterStreak("ZTKZQRKKZ", ["Z", "K", "Y"]), 3);
-  });
-});
-
 describe('#divisibleBy7Sort()', function () {
   it('should put all number divisible by 7 at the end.', function () {
     var result = divisibleBy7Sort([1, 4, 7, 43, 32, 14, 12]);
@@ -21,6 +13,14 @@ describe('#divisibleBy7Sort()', function () {
     result = divisibleBy7Sort([49, 40, 42, 51, 20, -7]);
     expected = [40, 51, 20, 49, 42, -7];
     assert.deepEqual(result, expected);
+  });
+});
+
+describe('#longestLetterStreak()', function () {
+  it('should return the longest consecutive searchLetters found in the str.', function () {
+    assert.equal(longestLetterStreak("ACCA", ["C"]), 2);
+    assert.equal(longestLetterStreak("YACCADCA", ["C", "A"]), 4);
+    assert.equal(longestLetterStreak("ZTKZQRKKZ", ["Z", "K", "Y"]), 3);
   });
 });
 
