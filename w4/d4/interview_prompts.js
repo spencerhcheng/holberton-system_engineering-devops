dcd871f81be09562b1aca5e36ad75b32d6abd0c1
Winnie <ngowinn@gmail.com>
2016-04-26 11:36:08 -0700

    Add instructions

--- w4/d4/interview_prompts.js
+++ w4/d4/interview_prompts.js
@@ -1,3 +1,34 @@
+/*
+### INSTRUCTIONS
+#
+# Welcome to the App Academy technical interview. Over the next 45 minutes, you
+# will be given a series of three programming questions. For each question, you
+# will have 15 minutes to write a solution in Ruby, Python, JavaScript, Perl,
+# PHP, or Java. We accept partial solutions, and overall logic is more important
+# to us than exact syntax, so try not to stress if your solution is incomplete
+# or imperfect. Take a deep breath and focus on your approach to the next
+# problem.
+#
+# Talk us through your approach as you work through the problem.  We need to know
+# how you communicate the way that you solve problems.  Some people prefer to talk
+# while they come to a solution, while other people prefer to walk us through
+# after they've done a bit of work.  Either way is fine.
+#
+# We want to evaluate you on your own work, so the interview is closed-book. You
+# are not permitted to consult any outside sources: don't ask others for help,
+# search the web for solutions, or refer to any notes or code you may have
+# written previously. Don't run your code in any REPL or interpreter, and don't
+# share these questions or your solutions with anyone else (even anonymously).
+# We take cheating very seriously, and violation is grounds for rejection or
+# dismissal from the course.
+#
+# That said, you may ask your interviewer any questions you might have. We are
+# more than happy to help clarify the question or let you know the right syntax
+# for the language you're using.
+#
+# Good luck!
+*/
+
 /******************************************************************************
 Write a function concatSandwich(word) which takes a string `word` and returns a
 function that takes three string arguments and returns `word` concatenated between
