dc38a5ec891214a4595ae89dacc43d5db7a4682b
Anthony Ira Ladson <aladsonjr@gmail.com>
2016-05-11 19:15:54 -0700

    Update review.js

--- w1/d5/review.js
+++ w1/d5/review.js
@@ -1,4 +1,4 @@
-/*
+/***************************************************************************
 Write a function `countScores(people)` that takes in an array of score
 objects (people) as its input. A score object has two key-value pairs:
 a name (string) and a score (number). `countScores(people)` should
@@ -24,31 +24,28 @@ var peeps = [
   {name: "Anthony", score: 2},
   {name: "Winnie", score: 2}
 ];
-
 countScores(peeps); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
-
-*/
+***************************************************************************/
 
 function countScores(people) {
   // your code here...
 }
 
-
-
-/*
-Write a function wordYeller(sentence) that takes in a sentence string as input. It
-should return the sentence where every word has an exclamation point behind it,
-except words that already have punctuation. Punctuation marks are: . , ! ? ; :
+/***************************************************************************
+Write a function wordYeller(sentence) that takes in a sentence string as 
+input. It should return the sentence where every word has an exclamation 
+point behind it, except words that already have punctuation. Punctuation 
+marks are ". , ! ? ; :"
 
 Example 1
 var sent = "Stop it now! Please, wont you stop?";
 var yelledWords = wordYeller(sent);
-yelledWords === "Stop! it! now! Please, wont! you! stop?";
+yelledWords === "Stop! it! now! Please, wont! you! stop?"; // => true
 
 Example 2
 var words = "Go to the store and grab the following: milk, bread, run, and cake";
-wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!";
-*/
+wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!"; // => true
+***************************************************************************/
 
 function wordYeller(sentence) {
   // your code here...
