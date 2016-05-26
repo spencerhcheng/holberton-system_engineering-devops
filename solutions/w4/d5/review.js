be198a5b65b82f6d1fac6635cea7b08acf03cdbd
Winnie Ngo <ngowinn@gmail.com>
2016-05-26 16:37:46 -0700

    add solutions

--- solutions/w4/d5/review.js
+++ solutions/w4/d5/review.js
@@ -1,20 +1,38 @@
 /******************************************************************************
 Write a function censor(sentence, curseWords) that censors the given sentence by
-replacing the vowels in curse words with "*".
+replacing the vowels in curse words with "*". Assume no punctuation.
 
 Use Array.prototype.forEach.
 
 Examples:
-censor("Darn you Harold you son of a gun!", ["darn", "gun"]); //=> "D*rn you Harold you son of a g*n!"
-censor("Schnikeys I don't give a diddly squat", ["schnikeys", "diddly", "squat"]) //=> "Schn*k*ys I don't give a d*ddly sq**t"
+censor("Darn you Harold you son of a gun", ["darn", "gun"]); //=> "D*rn you Harold you son of a g*n"
+censor("Schnikeys I dont give a diddly squat", ["schnikeys", "diddly", "squat"]) //=> "Schn*k*ys I dont give a d*ddly sq**t"
 ******************************************************************************/
 
-// your code here...
+function censor(sentence, curseWords) {
+  var censoredWords = [];
+
+  var words = sentence.split(" ");
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  words.forEach(function(word) {
+    if (curseWords.indexOf(word.toLowerCase()) !== -1) { // word is a curse word
+      vowels.forEach(function(vowel) { // replace each vowel with "*"
+        word = word.split(vowel).join("*");
+      });
+    }
+
+    censoredWords.push(word);
+  });
+
+  return censoredWords.join(" ");
+}
 
 /******************************************************************************
 Write a function greatestMapCreator(ary, cb1, cb2) which takes as an argument an
 array of numbers and two callbacks. It creates a new array with each element from ary
 "mapped" to a new element using the callback which returns a larger mapped value.
+Each callback take as its arguments the element, index and array.
 
 Use Array.prototype.forEach.
 
@@ -23,23 +41,35 @@ var ary1 = [1, 5, 10];
 var squareRootRounder = function(num) {
   return Math.round(Math.sqrt(num));
 };
-var half = function(num) { r
-  eturn num / 2;
+var half = function(num) {
+  return num / 2;
 };
-greatestMapCreator(ary1, squareRootRounder, half); //=> []
+greatestMapCreator(ary1, squareRootRounder, half); //=> [ 1, 2.5, 5 ]
 
 Example 2:
-var ary2 = [-10, 20, -100];
+var ary2 = [10, 20, -100];
 var absoluteValue = function(num) {
   return Math.abs(num);
 };
-var fraction = function(a, b) {
-  return a / b;
-};
-greatestMapCreator(ary2, absoluteValue, fraction); //=>
+var power = function(a, b) {
+  return Math.pow(a, b);
+}
+greatestMapCreator(ary2, absoluteValue, power); //=> [ 10, 20, 10000 ]
 ******************************************************************************/
 
-// your code here...
+function greatestMapCreator(ary, cb1, cb2) {
+  var mappedAry = [];
+
+  ary.forEach(function(num, idx, arry) {
+    var mappedEle1 = cb1(num, idx, arry);
+    var mappedEle2 = cb2(num, idx, arry);
+
+    var greatestEle = mappedEle1 > mappedEle2 ? mappedEle1 : mappedEle2;
+    mappedAry.push(greatestEle);
+  });
+
+  return mappedAry;
+}
 
 /******************************************************************************
 Isogram Matcher
--- w4/d5/review.js
+++ w4/d5/review.js
@@ -1,12 +1,12 @@
 /******************************************************************************
 Write a function censor(sentence, curseWords) that censors the given sentence by
-replacing the vowels in curse words with "*".
+replacing the vowels in curse words with "*". Assume no punctuation.
 
 Use Array.prototype.forEach.
 
 Examples:
-censor("Darn you Harold you son of a gun!", ["darn", "gun"]); //=> "D*rn you Harold you son of a g*n!"
-censor("Schnikeys I don't give a diddly squat", ["schnikeys", "diddly", "squat"]) //=> "Schn*k*ys I don't give a d*ddly sq**t"
+censor("Darn you Harold you son of a gun", ["darn", "gun"]); //=> "D*rn you Harold you son of a g*n"
+censor("Schnikeys I dont give a diddly squat", ["schnikeys", "diddly", "squat"]) //=> "Schn*k*ys I dont give a d*ddly sq**t"
 ******************************************************************************/
 
 // your code here...
@@ -15,6 +15,7 @@ censor("Schnikeys I don't give a diddly squat", ["schnikeys", "diddly", "squat"]
 Write a function greatestMapCreator(ary, cb1, cb2) which takes as an argument an
 array of numbers and two callbacks. It creates a new array with each element from ary
 "mapped" to a new element using the callback which returns a larger mapped value.
+Both callbacks take as arguments the element, index and array.
 
 Use Array.prototype.forEach.
 
@@ -23,20 +24,20 @@ var ary1 = [1, 5, 10];
 var squareRootRounder = function(num) {
   return Math.round(Math.sqrt(num));
 };
-var half = function(num) { r
-  eturn num / 2;
+var half = function(num) {
+  return num / 2;
 };
-greatestMapCreator(ary1, squareRootRounder, half); //=> []
+greatestMapCreator(ary1, squareRootRounder, half); //=> [ 1, 2.5, 5 ]
 
 Example 2:
-var ary2 = [-10, 20, -100];
+var ary2 = [10, 20, -100];
 var absoluteValue = function(num) {
   return Math.abs(num);
 };
-var fraction = function(a, b) {
-  return a / b;
-};
-greatestMapCreator(ary2, absoluteValue, fraction); //=>
+var power = function(a, b) {
+  return Math.pow(a, b);
+}
+greatestMapCreator(ary2, absoluteValue, power); //=> [ 10, 20, 10000 ]
 ******************************************************************************/
 
 // your code here...
