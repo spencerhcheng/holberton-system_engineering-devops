acb61bf031e6fd813ccb25137a80ec94074bead9
Winnie Ngo <ngowinn@gmail.com>
2016-05-26 14:47:53 -0700

    add forEach exercises

---
+++ solutions/w3/d4/solution.js
@@ -0,0 +1,29 @@
+function divisibleByNPairSum(ary, n) {
+  var results = [];
+
+  ary.forEach(function (n1, i) {
+    ary.forEach(function (n2, j) {
+      if ((j > i) && ((n1 + n2) % n === 0)) {
+        results.push([i, j]);
+      }
+    });
+  });
+
+  return results;
+}
+
+function peakFinder(array) {
+  var peaks = [];
+
+  array.forEach(function(num, idx) {
+    if ((idx === 0) && (num > array[idx + 1])) {
+      peaks.push(idx);
+    } else if ((idx === array.length - 1) && (num > array[idx - 1])) {
+      peaks.push(idx);
+    } else if ((num > array[idx + 1]) && (num > array[idx - 1])) {
+      peaks.push(idx);
+    }
+  });
+
+  return peaks;
+}
