26386e2e9f2b3a25441723f4beae349e5a9cbc7c
Joseph Burger <candyapplecorn@gmail.com>
2016-08-05 15:06:22 -0700

    Create problem_set.js

---
+++ w4/d4/problem_set/problem_set.js
@@ -0,0 +1,23 @@
+/*****************************************************************************
+            Refer to the day's problem set markdown file for
+            all problems' instructions and example output.
+*****************************************************************************/
+
+var isPrime  = function(num) {
+  // Your code here
+};
+
+var myMap    = function(arr, cb){
+  // Your code here
+};
+
+var myInject = function(arr, cb, accum){
+  // Your code here
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = {
+  myInject: myInject,
+  isPrime:  isPrime,
+  myMap:    myMap
+};
