bb4827e081234ca175adfab078a01df5c931784c
Alvin Zablan <azablan.dev@gmail.com>
2016-09-19 12:53:19 -0400

    fix #longWordCount()

--- solutions/w2/d4/solution.js
+++ solutions/w2/d4/solution.js
@@ -12,7 +12,7 @@ function longWordCount(sentence) {
   var count = 0;
   var words = sentence.split(" ");
 
-  for (var i = 0; i < sentence.length; i++) {
+  for (var i = 0; i < words.length; i++) {
     var word = words[i];
     if (word.length > 7) {
       count += 1;
