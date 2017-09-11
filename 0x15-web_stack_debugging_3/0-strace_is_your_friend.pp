36a5005f12bd60fe29270fb1bc1f657a2b949521
Spencer Cheng <spcheng0412@gmail.com>
2017-09-11 07:09:42 +0000

    Revert back yet again

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,13 @@
 # Creates index.html file in /tmp/holberton
---no-80chars-check
+
 file { '/var/www/html/index.html':
     ensure  => file,
     path    => '/var/www/html/index.html',
     mode    => '0644',
-    content => '<title>Holberton &#8211; Just another WordPress site</title>',
+    content => '<title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
+                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+        <p>Yet another bug by a Holberton student</p>'
 }
