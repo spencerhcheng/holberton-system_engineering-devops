82b2c42032bda344874eb63a23808401abd03b39
Spencer Cheng <spcheng0412@gmail.com>
2017-09-11 06:39:26 +0000

    Change single quote to double quote

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix typo with file extension
 exec { 'Replace line in /var/www/html/wp-settings.php':
     path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command     => 'sudo sed -ie 's/phpp/php/g' /var/www/html/wp-settings.php',
+    command     => "sudo sed -ie 's/phpp/php/g' /var/www/html/wp-settings.php",
   }
