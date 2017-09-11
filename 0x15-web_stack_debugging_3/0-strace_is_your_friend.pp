8b338d19c624c1f726944dfd52f74c49f6659fc4
Spencer Cheng <spcheng0412@gmail.com>
2017-09-11 06:38:40 +0000

    0-strace_is_your_friend: edit to have script fix typo associated with file extension

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,5 @@
-# Creates index.html file in /tmp/holberton
-
-file { '/var/www/html/index.html':
-    ensure  => file,
-    path    => '/var/www/html/index.html',
-    mode    => '0644',
-    content => '<title>Holberton &#8211; Just another WordPress site</title>
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
-        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
-                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
-        <p>Yet another bug by a Holberton student</p>'
-}
+# Fix typo with file extension
+exec { 'Replace line in /var/www/html/wp-settings.php':
+    path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command     => 'sudo sed -ie 's/phpp/php/g' /var/www/html/wp-settings.php',
+  }
