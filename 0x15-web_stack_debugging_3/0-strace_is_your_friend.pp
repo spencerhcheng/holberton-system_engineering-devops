34aee4e4b03a7fbbe4a9484e8f1ba6a8aaedaf03
Spencer Cheng <spcheng0412@gmail.com>
2017-09-11 07:01:07 +0000

    restart apache2 and add index.html

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,3 +3,8 @@ exec { 'Replace line in /var/www/html/wp-settings.php':
     path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
     command     => "sudo sed -ie 's/phpp/php/g' /var/www/html/wp-settings.php",
   }
+
+exec { 'Restart apache2':
+    command   => "sudo service apache2 restart; touch /var/www/html/index.html",
+    path      => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  }
