a7bcb9b49a4b0eab9247df3d1335cf0ecd8a08c3
Spencer Cheng <spcheng0412@gmail.com>
2017-09-17 23:56:24 +0000

    0-strace_is_your_friend: combine both methods

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,8 @@
-# Creates index.html file in /tmp/holberton
+# Fix typo with file extension
+exec { 'Replace line in /var/www/html/wp-settings.php':
+    path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command     => "sudo sed -ie 's/phpp/php/g' /var/www/html/wp-settings.php",
+  }
 
 file { '/var/www/html/index.html':
     ensure  => file,
