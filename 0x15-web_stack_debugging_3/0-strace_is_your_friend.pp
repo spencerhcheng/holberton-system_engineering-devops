40e0da0ef17720c2335c0ae8c4566308bb88ecd4
Spencer <spencerhcheng@gmail.com>
2017-09-19 00:08:29 +0000

    Replicate Julija's code

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fix typo with file extension
-exec { 'Replace line in /var/www/html/wp-settings.php':
-    path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command     => 'sudo sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+# fixing typo phpp instead of php in the wp-settings.php
+exec { 'replace phpp typo with php':
+  path    => '/usr/bin/:/usr/sbin/:/bin/',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
