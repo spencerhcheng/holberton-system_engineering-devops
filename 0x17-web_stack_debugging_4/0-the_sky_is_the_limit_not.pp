f3a108543aee5859d71a210527dc3a592a8d0443
spencerhcheng <spencerhcheng@gmail.com>
2017-09-19 18:26:36 +0000

    puppet-lint fix to reduce line below 80 chars

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increase worker processes
 exec { 'increase worker processes':
     path      => '/usr/bin/:/usr/sbin/:/bin/',
-    command   => 'sed -i "s/worker_processes 4;/worker_processes 12;/g" /etc/nginx/nginx.conf',
+    command   => 'sed -i "sses/ 4;/sses 12;/g" /etc/nginx/nginx.conf',
 }
 
 exec { 'Restart apache2':
