0f0b2c8860202827cee4db71558b91557e75bb05
spencerhcheng <spencerhcheng@gmail.com>
2017-09-21 20:59:34 +0000

    0-subs: initial commit

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+import sys
+import requests
+
 
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit
     """
-	url = sys.argv[1]	
+    sub_reddit = sys.argv[1]
+    url = 'https://www.reddit.com/r/'
+    reddit_url = url + sub_reddit + "/about.json"
+
+    r = requests.get(reddit_url, headers={'User-agent': 'Spencer'})
+    r_sub = r.json().get('data')
+    if r_sub is None:
+        return (0)
+
+    return(r_sub['subscribers'])
