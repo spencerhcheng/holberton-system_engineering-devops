2c8d6ebb6ee433dc137169d633ab958fd59affc0
spencerhcheng <spencerhcheng@gmail.com>
2017-09-21 22:46:41 +0000

    1-top_ten: initial commit

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    reddit_url = ('https://www.reddit.com/r/{}/hot.json'.format(subreddit))
+    r = requests.get(reddit_url, headers={'User-agent': 'Spencer'})
+
+    if r.status_code == 301:
+        return (0)
+
+    i = 0
+    r_sub = r.json().get('data').get('children')
+
+    if r_sub == []:
+        print (None)
+
+    for entries in r_sub:
+        for k, v in entries.get('data').items():
+            if k == 'title':
+                if i == 10:
+                    break
+                i += 1
+                print(v)
