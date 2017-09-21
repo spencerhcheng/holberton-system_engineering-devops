e1e49a4d9fb7a0b8038e686088a6c5b8534491c4
spencerhcheng <spencerhcheng@gmail.com>
2017-09-21 18:20:38 +0000

    0-subs.py: initial commit

---
+++ 0x18-api_advanced/0-subs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+	url = sys.argv[1]	
