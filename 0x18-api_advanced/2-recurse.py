2174f51ffa9fc735f4c8b8499fc612cb72f41b46
spencerhcheng <spencerhcheng@gmail.com>
2017-09-22 04:33:53 +0000

    Fix to account for non existant subreddits

--- 0x18-api_advanced/2-recurse.py
+++ 0x18-api_advanced/2-recurse.py
@@ -11,16 +11,21 @@ def recurse(subreddit, hot_list=[], after=""):
     if after is not None:
         reddit_url = ('https://www.reddit.com/r/{}/hot.json?&after={}'.
                       format(subreddit, after))
-        r = requests.get(reddit_url, headers={'User-agent': 'Spencer'})
-        if r.json() is None:
+        r = requests.get(reddit_url, headers={'User-agent': 'Spencer'},
+                         allow_redirects=False)
+        if not r.ok:
             return None
-        after = r.json().get('data', {}).get('after', {})
-        r_sub = r.json().get('data', {}).get('children', {})
-        if (after is None or r_sub is None or
-           len(after) == 0 or len(r_sub) == 0):
+        try:
+            r_sub = r.json().get('data', {}).get('children', {})
+        except:
             return None
+        if (r_sub is None or len(r_sub) == 0):
+            return None
+
         for entries in r_sub:
             title = entries.get('data', {}).get('title', {})
             hot_list.append(list)
+
+        after = r.json().get('data', {}).get('after', {})
         recurse(subreddit, hot_list, after)
     return (hot_list)
