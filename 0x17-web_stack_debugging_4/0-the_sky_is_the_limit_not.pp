42cf8d3979491ca99b01cf6dd2b94423242800e5
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 06:02:43 +0000

    0-the_sky_is_the_limit_not: initial commit

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Increase worker processes
 exec { 'increase worker processes':
     path      => '/usr/bin/:/usr/sbin/:/bin/',
-    command   => 'sed -i "s/sses 4;/sses 12;/g" /etc/nginx/nginx.conf',
+    command   => "sed -i 's/15/15000/g' /etc/default/nginx",
 }
 
-exec { 'Restart apache2':
+exec { 'Restart nginx':
     path      => '/usr/bin/:/usr/sbin/:/bin/',
     command   => 'sudo service nginx restart',
 }
